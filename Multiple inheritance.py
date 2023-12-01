8e4f86f4f478900d3e0583502d3f9b8e046de565
kvs0911 <131643041+kvs0911@users.noreply.github.com>
2023-12-01 08:21:22 +0530

    Create Multiple inheritance.py

---
+++ Multiple inheritance.py
@@ -0,0 +1,27 @@
+Aim: To demonstrate Multiple inheritance using classes
+Code:
+class Employee:
+    def __init__(self, name):
+        self.name = name
+
+     def show(self):
+        print(f"the name is {self.name}")
+
+
+class Dancer:
+    def __init__(self, dance):
+        self.dance = dance
+
+    def show(self):
+        print(f"the dance is {self.dance}")  
+
+
+class DancerEmployee(Employee, Dancer):
+    def __init__(self, name, dance):
+        self.name = name
+        self.dance = dance
+o = DancerEmployee("ABC", "Kathak")
+print(o.name)
+print(o.dance)
+o.show()
+print(DancerEmployee.mro())
